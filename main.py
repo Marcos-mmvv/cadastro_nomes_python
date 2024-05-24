59c058ff1c2bc58e4b6ba9395e597fcc50c38d06
marcos-mmvv <marcosveras.mv@hotmail.com>
2024-05-24 19:33:25 -0300

    Versão 1.0

---
+++ main.py
@@ -0,0 +1,14 @@
+# Lista de frutas
+
+frutas = []
+
+# Inseri novo item na lista
+
+nova_fruta = input('Informe o nome da nova fruta: ').capitalize()
+
+frutas.append(nova_fruta)
+
+for fruta in frutas:
+    print(fruta)
+
+
